37393e42e11c7a5e7c67a0dac2db6fa40e0ae2d9
Theron Packus <tlpackus@gmail.com>
2022-06-03 11:28:56 -0700

    update localhost server in package.json to connect as immbedded app and begin first attempts at pulling the event copper passes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Copper from "./Copper";
+import { useState } from 'react';
 
 function App() {
-  return (
+  const [getContext, handleGetContext] = useState(false);
+    return (
     <div className="App">
+    {getContext ? (
+      <Copper />
+    ) : (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={{border: '1px solid #fff'}} onClick={() => handleGetContext(true)}></button>
       </header>
+    )}
     </div>
   );
 }
---
+++ src/Copper.js
@@ -0,0 +1,23 @@
+import './Copper.css';
+// import Copper from 'copper-sdk';
+// import { useEffect } from 'react';
+
+// const CopperSDK = () => {
+//   const sdk = Copper.init();
+//   // useEffect(() => {
+//   //   async function handleGetContext() {
+//   //     const { context } = await sdk.getContext();
+//   //     const dataJsonObject = context.toJSON();
+//   //     const dataJavaScriptObject = JSON.parse(dataJsonObject);
+//   //     const customerEmails = dataJavaScriptObject.emails
+//   //     const customerFirstEmail = customerEmails[0].email;    
+//   // }, [])
+//   return (
+//     <div className="App">
+//       <div>sdk</div>
+//       <h1>Test</h1>
+//     </div>
+//   );
+// }
+
+// export default CopperSDK;
